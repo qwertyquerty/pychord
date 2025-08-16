adb142c780968cbbbffba32fec6778665df41619
Julian <thelastpocket@gmail.com>
2025-08-15 21:08:25 -0700

    formatting

--- pychord/chord.py
+++ pychord/chord.py
@@ -4,9 +4,14 @@ from pychord.const import *
 from pychord.interval import Interval, OCTAVE
 from pychord.note import Note
 
-class Chord():
+
+class Chord:
     """
     Describes a collection of `Note`s in 12TET
     """
-    def __init__(self, notes: set[Note] ): 
-        
+
+    def __init__(self, chord: Union[set[Note], str], root: Note = None):
+        """
+        `chord` can either be a set of `Note`s or a string definition
+        `root` is the root `Note` of the set of `Note`s; only required for passing in a set
+        """
